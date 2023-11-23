9ab624b427e9c69c246c003f54aed710da29fafd
jasmine217k <jasmine217k@gmail.com>
2023-11-23 12:06:18 +0530

    Adding:Repeating until player reached 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -7,12 +7,31 @@ public class SnakeAndLadder {
         //Player 1 starts from zero
         int playerOnePosition=0;
 
-        //diceRoll between 1 and 6
-        int diceRoll=(int)(Math.random()*10)%6 +1;
 
-        //Get options between noPlay, Snake and Ladder
-        int option=(int)(Math.random()*10)%3;
 
-        
+        while(playerOnePosition <100) {
+
+            //diceRoll between 1 and 6
+            int diceRoll=(int)(Math.random()*10)%6 +1;
+
+            //Get options between noPlay, Snake and Ladder
+            int option = (int) (Math.random() * 10) % 3;
+
+
+            if(option==0) //No Play
+                continue;
+            else if(option==1) //Snake
+            {
+                playerOnePosition-=diceRoll;
+                if(playerOnePosition <0)
+                    playerOnePosition=0;
+            }
+            else {
+                playerOnePosition+=diceRoll;
+            }
+            
+        }
+
+
         }
     }
